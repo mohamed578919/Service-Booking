afa130f4d2c6f2abc2179fd1af3637b61b5fa66f
Mohamed <gobaa9346@gmail.com>
2025-09-24 22:37:34 +0300

    back-End

--- ServiceBooking/obj/Debug/net8.0/ServiceBooking.AssemblyInfo.cs
+++ ServiceBooking/obj/Debug/net8.0/ServiceBooking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2756bf5a405262adb15f547af506fe6919b2d0a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21f2150c7c29b5058d2f235f91fa7ed9e25e12d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
